01812ebad61bf84f8372def3ba1ea9e7df8abec8
Dakutsuki <demomega9@gmail.com>
2024-11-04 12:11:55 +0100

    go

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -2,14 +2,11 @@ import React from 'react'
 import '../../index.css'
 const About = () => {
   return (
-    <div class="about-container">
+    <div class="about-container" id='font'>
     <h1>Foresight. Vision. Execution.</h1>
 
     <h4>A visionary real estate developer and investor with a proven track record of
-         delivering high-quality projects that exceed expectations. As the Founder
-          and Managing Partner of Foresight Reality Partners, he has established
-           himself as a leader in the industry, Known for his innovative approach,
-             strategic thinking, and commitment to excellence.</h4>
+         delivering high-quality projects that exceed expectations</h4>
 
          <button class="btn">See More</button>
   </div>
